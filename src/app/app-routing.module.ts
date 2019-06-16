369e2680286ae8c48f591abcb1c715f0eb69c5f7
Oleksandr Poliakov <oleksandr_poliakov@epam.com>
2019-06-16 14:51:33 +0300

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NotesComponent } from './notes/notes.component';
+import { NoteComponent } from './note/note.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent,
+  },
+  { path: '',
+    redirectTo: '/notes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'notes/:id',
+    component: NoteComponent,
+  },
+  {
+    path: 'archive',
+    component: ArchiveComponent,
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { NoteComponent } from './note/note.component';
+import { NotesComponent } from './notes/notes.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArchiveComponent,
+    NoteComponent,
+    NotesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
